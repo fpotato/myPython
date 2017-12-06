12f474b9d60eacfdae300409559a7ca75d43d05c
fpotato <opensourcedevil@hotmail.com>
2017-12-06 16:52:47 +0800

    1

--- test.py
+++ test.py
@@ -7,23 +7,8 @@ sys.setdefaultencoding('utf-8')
 
 import bisect 
 
-haystack = [1,4,5,6,8,12,15,20,21,23,23,26,29,30]
-needles = [0,1,2,5,8,10,22,23,29,30,31]
+def grade(score, breakpoints=[60,70,80,90], grades = 'FDCBA'):
+	i = bisect.bisect(breakpoints,score)
+	return grade[i]
 
-row_fmt = '{0:2d} @ {1:2d}    {2}{0:<2d}'
-
-def demo(bisect_fn):
-	for needle in reversed(needles):
-		position = bisect_fn(haystack,needle)
-		offset = position * '  |'
-		print(row_fmt.format(needle,position,offset))
-
-if __name__ == '__main__':
-	if sys.argv[-1] == 'left':
-		bisect_fn = bisect.bisect_left
-	else:
-		bisect_fn = bisect.bisect
-
-	print 'DEMO:', bisect_fn.__name__
-	print 'haystack ->',' '.join('%2d' % n for n in haystack)
-	demo(bisect_fn)
+print(grade(10))
